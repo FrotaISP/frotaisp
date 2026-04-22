3905ec5943dd9e27aebd5542cb66627610e4198f
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-22 14:03:10 -0300

    Fix trip company migration dependency

--- apps/trips/migrations/0002_trip_company.py
+++ apps/trips/migrations/0002_trip_company.py
@@ -19,7 +19,7 @@ def assign_company_from_vehicle(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0001_initial'),
+        ('accounts', '0005_company_userprofile_company'),
         ('drivers', '0002_driver_company'),
         ('vehicles', '0002_vehicle_company'),
         ('trips', '0001_initial'),
