f466f9dbd0f2e30184c79eda9e4f2e6bc65802c2
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-22 13:29:02 -0300

    Scope fuel views by company

--- apps/fuel/views.py
+++ apps/fuel/views.py
@@ -2,39 +2,39 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from apps.core.mixins import FuelRegisterMixin, ManagerRequiredMixin
+from apps.core.mixins import FuelRegisterMixin, ManagerRequiredMixin, TenantFormMixin, TenantQuerySetMixin
 from .models import FuelRecord
 from .forms import FuelRecordForm
 
 
-class FuelRecordListView(LoginRequiredMixin, ListView):
+class FuelRecordListView(TenantQuerySetMixin, LoginRequiredMixin, ListView):
     model = FuelRecord
     template_name = 'fuel/fuelrecord_list.html'
     context_object_name = 'fuel_records'
     paginate_by = 15
 
 
-class FuelRecordDetailView(LoginRequiredMixin, DetailView):
+class FuelRecordDetailView(TenantQuerySetMixin, LoginRequiredMixin, DetailView):
     model = FuelRecord
     template_name = 'fuel/fuelrecord_detail.html'
     context_object_name = 'record'
 
 
-class FuelRecordCreateView(FuelRegisterMixin, CreateView):
+class FuelRecordCreateView(TenantFormMixin, FuelRegisterMixin, CreateView):
     model = FuelRecord
     form_class = FuelRecordForm
     template_name = 'fuel/fuelrecord_form.html'
     success_url = reverse_lazy('fuel:list')
 
 
-class FuelRecordUpdateView(FuelRegisterMixin, UpdateView):
+class FuelRecordUpdateView(TenantFormMixin, TenantQuerySetMixin, FuelRegisterMixin, UpdateView):
     model = FuelRecord
     form_class = FuelRecordForm
     template_name = 'fuel/fuelrecord_form.html'
     success_url = reverse_lazy('fuel:list')
 
 
-class FuelRecordDeleteView(ManagerRequiredMixin, DeleteView):
+class FuelRecordDeleteView(TenantQuerySetMixin, ManagerRequiredMixin, DeleteView):
     model = FuelRecord
     template_name = 'fuel/fuelrecord_confirm_delete.html'
     success_url = reverse_lazy('fuel:list')
