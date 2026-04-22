8336846a20186ab5be272c7557729eec32cbb510
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-22 16:27:57 -0300

    Add tracking map routes

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -6,4 +6,6 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.dashboard_view, name='index'),
+    path('rastreamento/', views.tracking_map_view, name='tracking'),
+    path('rastreamento/dados/', views.tracking_positions_api, name='tracking_positions'),
 ]
