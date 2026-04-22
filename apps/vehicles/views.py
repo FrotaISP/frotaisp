533202cb1d9192cd9d6eb585bfc578611baa6930
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-22 13:28:57 -0300

    Scope vehicle views by company

--- apps/vehicles/views.py
+++ apps/vehicles/views.py
@@ -2,40 +2,40 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from apps.core.mixins import OperatorRequiredMixin, ManagerRequiredMixin, ProtectedDeleteMixin
+from apps.core.mixins import OperatorRequiredMixin, ManagerRequiredMixin, ProtectedDeleteMixin, TenantFormMixin, TenantQuerySetMixin
 from .models import Vehicle
 from .forms import VehicleForm
 
 
-class VehicleListView(LoginRequiredMixin, ListView):
+class VehicleListView(TenantQuerySetMixin, LoginRequiredMixin, ListView):
     model = Vehicle
     template_name = 'vehicles/vehicle_list.html'
     context_object_name = 'vehicles'
     paginate_by = 10
 
 
-class VehicleDetailView(LoginRequiredMixin, DetailView):
+class VehicleDetailView(TenantQuerySetMixin, LoginRequiredMixin, DetailView):
     model = Vehicle
     template_name = 'vehicles/vehicle_detail.html'
     context_object_name = 'vehicle'
 
 
-class VehicleCreateView(OperatorRequiredMixin, CreateView):
+class VehicleCreateView(TenantFormMixin, OperatorRequiredMixin, CreateView):
     model = Vehicle
     form_class = VehicleForm
     template_name = 'vehicles/vehicle_form.html'
     success_url = reverse_lazy('vehicles:list')
 
 
-class VehicleUpdateView(OperatorRequiredMixin, UpdateView):
+class VehicleUpdateView(TenantFormMixin, TenantQuerySetMixin, OperatorRequiredMixin, UpdateView):
     model = Vehicle
     form_class = VehicleForm
     template_name = 'vehicles/vehicle_form.html'
     success_url = reverse_lazy('vehicles:list')
 
 
-class VehicleDeleteView(ProtectedDeleteMixin, ManagerRequiredMixin, DeleteView):
+class VehicleDeleteView(TenantQuerySetMixin, ProtectedDeleteMixin, ManagerRequiredMixin, DeleteView):
     model = Vehicle
     template_name = 'vehicles/vehicle_confirm_delete.html'
     success_url = reverse_lazy('vehicles:list')
-    protected_error_message = 'Este veículo não pode ser excluído porque possui viagens, abastecimentos ou manutenções vinculadas.'
+    protected_error_message = 'Este veiculo nao pode ser excluido porque possui viagens, abastecimentos ou manutencoes vinculadas.'
