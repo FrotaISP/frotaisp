9de079de74deefe8ede8648b0a8ac2c0a42cf283
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-22 13:24:18 -0300

    Scope maintenance records by company

--- apps/maintenance/models.py
+++ apps/maintenance/models.py
@@ -10,31 +10,41 @@ class Maintenance(TimeStampedModel):
         ('E', 'Emergencial'),
     ]
 
+    company = models.ForeignKey(
+        'accounts.Company',
+        on_delete=models.PROTECT,
+        related_name='maintenances',
+        verbose_name='Empresa',
+        null=True,
+        blank=True,
+    )
     vehicle = models.ForeignKey(
         'vehicles.Vehicle',
         on_delete=models.PROTECT,
         related_name='maintenances',
-        verbose_name='Veículo'
+        verbose_name='Veiculo'
     )
     type = models.CharField('Tipo', max_length=1, choices=MAINTENANCE_TYPES)
     date = models.DateField('Data')
-    description = models.TextField('Descrição')
+    description = models.TextField('Descricao')
     cost = models.DecimalField('Custo', max_digits=8, decimal_places=2)
     odometer = models.IntegerField('Km no Momento')
     workshop = models.CharField('Oficina', max_length=100)
-    next_alert_km = models.IntegerField('Alerta próx. manutenção (km)', null=True, blank=True)
-    next_alert_date = models.DateField('Alerta próx. manutenção (data)', null=True, blank=True)
+    next_alert_km = models.IntegerField('Alerta prox. manutencao (km)', null=True, blank=True)
+    next_alert_date = models.DateField('Alerta prox. manutencao (data)', null=True, blank=True)
     invoice = models.FileField('Nota Fiscal', upload_to='maintenance_invoices/', blank=True)
 
     class Meta:
-        verbose_name = 'Manutenção'
-        verbose_name_plural = 'Manutenções'
+        verbose_name = 'Manutencao'
+        verbose_name_plural = 'Manutencoes'
         ordering = ['-date']
 
     def __str__(self):
         return f"{self.vehicle.plate} - {self.get_type_display()} - {self.date}"
 
     def save(self, *args, **kwargs):
+        if not self.company_id and self.vehicle_id:
+            self.company = self.vehicle.company
         super().save(*args, **kwargs)
 
         if self.odometer > self.vehicle.current_odometer:
