73f2f007093d0d35c1ddadd1945c0cf53fcd3d29
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-22 14:18:09 -0300

    Add preventive maintenance plan views

--- apps/maintenance/views.py
+++ apps/maintenance/views.py
@@ -3,8 +3,8 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from apps.core.mixins import MaintenanceRegisterMixin, ManagerRequiredMixin, TenantFormMixin, TenantQuerySetMixin
-from .models import Maintenance
-from .forms import MaintenanceForm
+from .models import Maintenance, PreventiveMaintenancePlan
+from .forms import MaintenanceForm, PreventiveMaintenancePlanForm
 
 
 class MaintenanceListView(TenantQuerySetMixin, LoginRequiredMixin, ListView):
@@ -38,3 +38,39 @@ class MaintenanceDeleteView(TenantQuerySetMixin, ManagerRequiredMixin, DeleteVie
     model = Maintenance
     template_name = 'maintenance/maintenance_confirm_delete.html'
     success_url = reverse_lazy('maintenance:list')
+
+
+class PreventiveMaintenancePlanListView(TenantQuerySetMixin, LoginRequiredMixin, ListView):
+    model = PreventiveMaintenancePlan
+    template_name = 'maintenance/plan_list.html'
+    context_object_name = 'plans'
+    paginate_by = 15
+
+    def get_queryset(self):
+        return super().get_queryset().select_related('vehicle')
+
+
+class PreventiveMaintenancePlanDetailView(TenantQuerySetMixin, LoginRequiredMixin, DetailView):
+    model = PreventiveMaintenancePlan
+    template_name = 'maintenance/plan_detail.html'
+    context_object_name = 'plan'
+
+
+class PreventiveMaintenancePlanCreateView(TenantFormMixin, MaintenanceRegisterMixin, CreateView):
+    model = PreventiveMaintenancePlan
+    form_class = PreventiveMaintenancePlanForm
+    template_name = 'maintenance/plan_form.html'
+    success_url = reverse_lazy('maintenance:plans')
+
+
+class PreventiveMaintenancePlanUpdateView(TenantFormMixin, TenantQuerySetMixin, MaintenanceRegisterMixin, UpdateView):
+    model = PreventiveMaintenancePlan
+    form_class = PreventiveMaintenancePlanForm
+    template_name = 'maintenance/plan_form.html'
+    success_url = reverse_lazy('maintenance:plans')
+
+
+class PreventiveMaintenancePlanDeleteView(TenantQuerySetMixin, ManagerRequiredMixin, DeleteView):
+    model = PreventiveMaintenancePlan
+    template_name = 'maintenance/plan_confirm_delete.html'
+    success_url = reverse_lazy('maintenance:plans')
