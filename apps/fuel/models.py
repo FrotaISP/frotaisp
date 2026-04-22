30a35415590b9003b52d91cbac7835d937b76ffa
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-22 13:24:16 -0300

    Scope fuel records by company

--- apps/fuel/models.py
+++ apps/fuel/models.py
@@ -4,15 +4,23 @@ from apps.core.models import TimeStampedModel
 
 
 class FuelRecord(TimeStampedModel):
+    company = models.ForeignKey(
+        'accounts.Company',
+        on_delete=models.PROTECT,
+        related_name='fuel_records',
+        verbose_name='Empresa',
+        null=True,
+        blank=True,
+    )
     vehicle = models.ForeignKey(
         'vehicles.Vehicle',
         on_delete=models.PROTECT,
         related_name='fuel_records',
-        verbose_name='Veículo'
+        verbose_name='Veiculo'
     )
     date = models.DateField('Data')
     liters = models.DecimalField('Litros', max_digits=8, decimal_places=2)
-    price_per_liter = models.DecimalField('Preço por Litro', max_digits=6, decimal_places=2)
+    price_per_liter = models.DecimalField('Preco por Litro', max_digits=6, decimal_places=2)
     total_cost = models.DecimalField('Custo Total', max_digits=8, decimal_places=2, editable=False)
     odometer = models.IntegerField('Km no Abastecimento')
     gas_station = models.CharField('Posto', max_length=100)
@@ -24,6 +32,8 @@ class FuelRecord(TimeStampedModel):
         ordering = ['-date', '-created_at']
 
     def save(self, *args, **kwargs):
+        if not self.company_id and self.vehicle_id:
+            self.company = self.vehicle.company
         self.total_cost = self.liters * self.price_per_liter
         super().save(*args, **kwargs)
 
