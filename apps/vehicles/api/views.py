24ed0bf87ccf752211947e440e88f2b5aa87c828
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-22 13:32:51 -0300

    Scope vehicle API by company

--- apps/vehicles/api/views.py
+++ apps/vehicles/api/views.py
@@ -1,8 +1,17 @@
 # apps/vehicles/api/views.py
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from apps.core.mixins import get_user_company, scope_queryset_for_user
 from apps.vehicles.models import Vehicle
 from apps.vehicles.serializers import VehicleSerializer
 
+
 class VehicleViewSet(viewsets.ModelViewSet):
-    queryset = Vehicle.objects.filter(is_active=True)
     serializer_class = VehicleSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return scope_queryset_for_user(Vehicle.objects.filter(is_active=True), self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(company=get_user_company(self.request.user))
