d3741b8651e57707d36d06521d7094baceee8d39
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-22 14:17:32 -0300

    Add document and checklist views

--- apps/vehicles/views.py
+++ apps/vehicles/views.py
@@ -3,8 +3,8 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from apps.core.mixins import OperatorRequiredMixin, ManagerRequiredMixin, ProtectedDeleteMixin, TenantFormMixin, TenantQuerySetMixin
-from .models import Vehicle
-from .forms import VehicleForm
+from .models import Vehicle, VehicleChecklist, VehicleDocument
+from .forms import VehicleChecklistForm, VehicleDocumentForm, VehicleForm
 
 
 class VehicleListView(TenantQuerySetMixin, LoginRequiredMixin, ListView):
@@ -39,3 +39,75 @@ class VehicleDeleteView(TenantQuerySetMixin, ProtectedDeleteMixin, ManagerRequir
     template_name = 'vehicles/vehicle_confirm_delete.html'
     success_url = reverse_lazy('vehicles:list')
     protected_error_message = 'Este veiculo nao pode ser excluido porque possui viagens, abastecimentos ou manutencoes vinculadas.'
+
+
+class VehicleDocumentListView(TenantQuerySetMixin, LoginRequiredMixin, ListView):
+    model = VehicleDocument
+    template_name = 'vehicles/document_list.html'
+    context_object_name = 'documents'
+    paginate_by = 15
+
+    def get_queryset(self):
+        return super().get_queryset().select_related('vehicle', 'driver', 'driver__user')
+
+
+class VehicleDocumentDetailView(TenantQuerySetMixin, LoginRequiredMixin, DetailView):
+    model = VehicleDocument
+    template_name = 'vehicles/document_detail.html'
+    context_object_name = 'document'
+
+
+class VehicleDocumentCreateView(TenantFormMixin, OperatorRequiredMixin, CreateView):
+    model = VehicleDocument
+    form_class = VehicleDocumentForm
+    template_name = 'vehicles/document_form.html'
+    success_url = reverse_lazy('vehicles:documents')
+
+
+class VehicleDocumentUpdateView(TenantFormMixin, TenantQuerySetMixin, OperatorRequiredMixin, UpdateView):
+    model = VehicleDocument
+    form_class = VehicleDocumentForm
+    template_name = 'vehicles/document_form.html'
+    success_url = reverse_lazy('vehicles:documents')
+
+
+class VehicleDocumentDeleteView(TenantQuerySetMixin, ManagerRequiredMixin, DeleteView):
+    model = VehicleDocument
+    template_name = 'vehicles/document_confirm_delete.html'
+    success_url = reverse_lazy('vehicles:documents')
+
+
+class VehicleChecklistListView(TenantQuerySetMixin, LoginRequiredMixin, ListView):
+    model = VehicleChecklist
+    template_name = 'vehicles/checklist_list.html'
+    context_object_name = 'checklists'
+    paginate_by = 15
+
+    def get_queryset(self):
+        return super().get_queryset().select_related('vehicle', 'driver', 'driver__user')
+
+
+class VehicleChecklistDetailView(TenantQuerySetMixin, LoginRequiredMixin, DetailView):
+    model = VehicleChecklist
+    template_name = 'vehicles/checklist_detail.html'
+    context_object_name = 'checklist'
+
+
+class VehicleChecklistCreateView(TenantFormMixin, OperatorRequiredMixin, CreateView):
+    model = VehicleChecklist
+    form_class = VehicleChecklistForm
+    template_name = 'vehicles/checklist_form.html'
+    success_url = reverse_lazy('vehicles:checklists')
+
+
+class VehicleChecklistUpdateView(TenantFormMixin, TenantQuerySetMixin, OperatorRequiredMixin, UpdateView):
+    model = VehicleChecklist
+    form_class = VehicleChecklistForm
+    template_name = 'vehicles/checklist_form.html'
+    success_url = reverse_lazy('vehicles:checklists')
+
+
+class VehicleChecklistDeleteView(TenantQuerySetMixin, ManagerRequiredMixin, DeleteView):
+    model = VehicleChecklist
+    template_name = 'vehicles/checklist_confirm_delete.html'
+    success_url = reverse_lazy('vehicles:checklists')
