70aece047893d69051dc6d926ed940d6138f7927
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-22 13:23:12 -0300

    Assign default company on automatic profiles

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -2,12 +2,18 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import UserProfile
+from .models import Company, UserProfile
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    """Cria automaticamente um UserProfile quando um novo User é criado."""
+    """Cria automaticamente um UserProfile quando um novo User e criado."""
     if created:
         role = 'admin' if instance.is_superuser else 'viewer'
-        UserProfile.objects.get_or_create(user=instance, defaults={'role': role})
+        UserProfile.objects.get_or_create(
+            user=instance,
+            defaults={
+                'role': role,
+                'company': Company.get_default_company(),
+            },
+        )
