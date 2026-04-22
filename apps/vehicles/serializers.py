7179efe2e9465261d60ed8df0bd45d65d375372a
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-22 13:33:03 -0300

    Protect company field in vehicle API serializer

--- apps/vehicles/serializers.py
+++ apps/vehicles/serializers.py
@@ -2,7 +2,9 @@
 from rest_framework import serializers
 from .models import Vehicle
 
+
 class VehicleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vehicle
         fields = '__all__'
+        read_only_fields = ['company']
