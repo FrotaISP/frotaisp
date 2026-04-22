3e807c14e1d364e678942df751c1be9e854e628d
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-22 13:29:04 -0300

    Scope maintenance views by company

--- apps/maintenance/views.py
+++ apps/maintenance/views.py
@@ -2,39 +2,39 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from apps.core.mixins import MaintenanceRegisterMixin, ManagerRequiredMixin
+from apps.core.mixins import MaintenanceRegisterMixin, ManagerRequiredMixin, TenantFormMixin, TenantQuerySetMixin
 from .models import Maintenance
 from .forms import MaintenanceForm
 
 
-class MaintenanceListView(LoginRequiredMixin, ListView):
+class MaintenanceListView(TenantQuerySetMixin, LoginRequiredMixin, ListView):
     model = Maintenance
     template_name = 'maintenance/maintenance_list.html'
     context_object_name = 'maintenances'
     paginate_by = 10
 
 
-class MaintenanceDetailView(LoginRequiredMixin, DetailView):
+class MaintenanceDetailView(TenantQuerySetMixin, LoginRequiredMixin, DetailView):
     model = Maintenance
     template_name = 'maintenance/maintenance_detail.html'
     context_object_name = 'maintenance'
 
 
-class MaintenanceCreateView(MaintenanceRegisterMixin, CreateView):
+class MaintenanceCreateView(TenantFormMixin, MaintenanceRegisterMixin, CreateView):
     model = Maintenance
     form_class = MaintenanceForm
     template_name = 'maintenance/maintenance_form.html'
     success_url = reverse_lazy('maintenance:list')
 
 
-class MaintenanceUpdateView(MaintenanceRegisterMixin, UpdateView):
+class MaintenanceUpdateView(TenantFormMixin, TenantQuerySetMixin, MaintenanceRegisterMixin, UpdateView):
     model = Maintenance
     form_class = MaintenanceForm
     template_name = 'maintenance/maintenance_form.html'
     success_url = reverse_lazy('maintenance:list')
 
 
-class MaintenanceDeleteView(ManagerRequiredMixin, DeleteView):
+class MaintenanceDeleteView(TenantQuerySetMixin, ManagerRequiredMixin, DeleteView):
     model = Maintenance
     template_name = 'maintenance/maintenance_confirm_delete.html'
     success_url = reverse_lazy('maintenance:list')
