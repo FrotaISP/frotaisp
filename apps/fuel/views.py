c6006a55424a7c76e4f0e2deb351ac87cf34a91e
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-24 08:39:04 -0300

    Sync manager fuel dashboard improvements from local

--- apps/fuel/views.py
+++ apps/fuel/views.py
@@ -1,5 +1,6 @@
 # apps/fuel/views.py
 from django.urls import reverse_lazy
+from django.db.models import Sum
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from apps.core.mixins import FuelRegisterMixin, ManagerRequiredMixin, TenantFormMixin, TenantQuerySetMixin
@@ -13,11 +14,28 @@ class FuelRecordListView(TenantQuerySetMixin, LoginRequiredMixin, ListView):
     context_object_name = 'fuel_records'
     paginate_by = 15
 
+    def get_queryset(self):
+        return super().get_queryset().select_related('vehicle')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        records = self.get_queryset()
+        context.update({
+            'records_total': records.count(),
+            'records_with_receipt': records.exclude(receipt='').count(),
+            'records_with_photo': records.exclude(photo='').count(),
+            'records_total_cost': records.aggregate(total=Sum('total_cost'))['total'] or 0,
+        })
+        return context
+
 
 class FuelRecordDetailView(TenantQuerySetMixin, LoginRequiredMixin, DetailView):
     model = FuelRecord
     template_name = 'fuel/fuelrecord_detail.html'
     context_object_name = 'record'
+
+    def get_queryset(self):
+        return super().get_queryset().select_related('vehicle')
 
 
 class FuelRecordCreateView(TenantFormMixin, FuelRegisterMixin, CreateView):
