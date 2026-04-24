e8da6eb81c78cb08ad069143c135968bf4b8e8fd
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-24 09:03:04 -0300

    Remove legacy root vehicles api views

--- vehicles/api/views.py
+++
@@ -1,8 +0,0 @@
-# apps/vehicles/api/views.py
-from rest_framework import viewsets
-from apps.vehicles.models import Vehicle
-from apps.vehicles.serializers import VehicleSerializer
-
-class VehicleViewSet(viewsets.ModelViewSet):
-    queryset = Vehicle.objects.filter(is_active=True)
-    serializer_class = VehicleSerializer
