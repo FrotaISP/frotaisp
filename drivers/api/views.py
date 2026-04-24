a50c88f0b248954823e6acb87f1678c20351663b
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-24 08:59:14 -0300

    Remove legacy root app files moved under apps package

--- drivers/api/views.py
+++
@@ -1,8 +0,0 @@
-# apps/drivers/api/views.py
-from rest_framework import viewsets
-from apps.drivers.models import Driver
-from apps.drivers.serializers import DriverSerializer
-
-class DriverViewSet(viewsets.ModelViewSet):
-    queryset = Driver.objects.filter(is_available=True)
-    serializer_class = DriverSerializer
