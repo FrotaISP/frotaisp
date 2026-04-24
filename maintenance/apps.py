7380185ae978477c4a5a0627e916b66ca8eafd37
katoonnoass <69221308+katoonnoass@users.noreply.github.com>
2026-04-24 09:00:58 -0300

    Remove legacy root app files moved under apps package

--- maintenance/apps.py
+++
@@ -1,7 +0,0 @@
-# apps/maintenance/apps.py
-from django.apps import AppConfig
-
-class MaintenanceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.maintenance'
-    verbose_name = 'Manutenções'
